0609187a7587b6699b70431126ef50c2809bdd54
DobosAlbert <dobos.albert123@gmail.com>
2023-03-23 21:12:20 +0200

    update

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,7 +1,29 @@
 import "./TaskCard.css";
 
 function create() {
-    return <div className="card-wrapper">First text</div>;
+    return (<div className="card-wrapper">
+
+       <div className="card-header">
+        <p className="task-id">T-1</p>
+        <div className="badge">
+            <p>Todo</p>
+        </div>
+        
+
+       </div>
+       <div className="card-content">
+          <p>Create a Design System for Enum Workspace</p>
+        </div>
+        
+        <div className="card-footer">   
+            <div>
+            <p>Due Date</p>
+            <p>23/01/2021</p>
+            </div>
+        </div>
+
+
+    </div>);
 }
 
 export default create;
